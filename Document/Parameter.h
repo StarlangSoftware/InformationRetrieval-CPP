e9d69d9ea98c700842626621af665763c280d942
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-07-21 20:11:50 +0300

    Initial repository translation.

---
+++ Document/Parameter.h
@@ -0,0 +1,63 @@
+//
+// Created by Olcay Taner YILDIZ on 21.07.2022.
+//
+
+#ifndef INFORMATIONRETRIEVAL_PARAMETER_H
+#define INFORMATIONRETRIEVAL_PARAMETER_H
+
+
+#include "IndexType.h"
+#include "Dictionary.h"
+#include "MorphologicalDisambiguator.h"
+#include "FsmMorphologicalAnalyzer.h"
+
+class Parameter {
+private:
+    IndexType indexType = IndexType::INVERTED_INDEX;
+    Comparator wordComparator = Comparator::TURKISH;
+    bool indexesFromFile = false;
+    MorphologicalDisambiguator* disambiguator = nullptr;
+    FsmMorphologicalAnalyzer fsm;
+    bool _normalizeDocument = false;
+    bool phraseIndex = true;
+    bool positionalIndex = true;
+    bool nGramIndex = true;
+    bool indexInDisk = false;
+    bool dictionaryInDisk = false;
+    bool _limitNumberOfDocumentsLoaded = false;
+    int documentLimit = 1000;
+    int wordLimit = 10000;
+public:
+    Parameter() = default;
+    IndexType getIndexType();
+    Comparator getWordComparator();
+    bool loadIndexesFromFile();
+    MorphologicalDisambiguator* getDisambiguator();
+    FsmMorphologicalAnalyzer getFsm();
+    bool constructPhraseIndex();
+    bool normalizeDocument();
+    bool constructPositionalIndex();
+    bool constructNGramIndex();
+    bool constructIndexInDisk();
+    bool limitNumberOfDocumentsLoaded();
+    int getDocumentLimit();
+    bool constructDictionaryInDisk();
+    int getWordLimit();
+    void setIndexType(IndexType indexType);
+    void setWordComparator(Comparator wordComparator);
+    void setLoadIndexesFromFile(bool loadIndexesFromFile);
+    void setDisambiguator(MorphologicalDisambiguator* disambiguator);
+    void setFsm(FsmMorphologicalAnalyzer fsm);
+    void setNormalizeDocument(bool normalizeDocument);
+    void setPhraseIndex(bool phraseIndex);
+    void setPositionalIndex(bool positionalIndex);
+    void setNGramIndex(bool nGramIndex);
+    void setConstructIndexInDisk(bool constructIndexInDisk);
+    void setLimitNumberOfDocumentsLoaded(bool limitNumberOfDocumentsLoaded);
+    void setDocumentLimit(int documentLimit);
+    void setConstructDictionaryInDisk(bool constructDictionaryInDisk);
+    void setWordLimit(int wordLimit);
+};
+
+
+#endif //INFORMATIONRETRIEVAL_PARAMETER_H
